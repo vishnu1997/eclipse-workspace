2ef0abd285a3342b9940624dca8309544c4f452b
Vishnu Vardhan Reddy <reddy.vishnu1997@gmail.com>
2018-03-24 10:12:22 +0530

    Initial commit

---
+++ WebSpring/src/main/java/App.java
@@ -0,0 +1,15 @@
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"Spring-Module.xml");
+
+		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+		obj.setName("maggi");
+		obj.printHello();
+	}
+}
